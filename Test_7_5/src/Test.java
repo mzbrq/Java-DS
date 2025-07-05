1d9b3275501df5a98d89f97e93e34654a87c5603
mzbrq <1404461081@qq.com>
2025-07-05 22:49:58 +0800

    刷题

---
+++ Test_7_5/src/Test.java
@@ -0,0 +1,110 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("刷题");
+    }
+
+    //反转二叉树
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+
+    //相同的树
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) &&
+                    isSameTree(p.right, q.right);
+        }
+    }
+
+    //对称二叉树
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetricChild(root.left, root.right);
+    }
+
+    public boolean isSymmetricChild(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) {
+            return true;
+        } else if (leftNode == null || rightNode == null) {
+            return false;
+        } else if (leftNode.val != rightNode.val) {
+            return false;
+        } else {
+            return isSymmetricChild(leftNode.left, rightNode.right) &&
+                    isSymmetricChild(leftNode.right, rightNode.left);
+        }
+    }
+
+
+    //另一颗树的子树
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+
+        if (isSametree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSametree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSametree(p.left, q.left) &&
+                    isSametree(p.right, q.right);
+        }
+    }
+
+
+    //平衡二叉树
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return getHeight(root) >= 0;
+
+
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+
+}
